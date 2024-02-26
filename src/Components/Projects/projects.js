ffb93fa044f26b7ac8795d8bc7d432d3d9cb32d4
WaterProofComputer <waterproofcomputer@gmail.com>
2024-02-26 15:55:12 +0200

    pieni jatko

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -8,8 +8,16 @@ function Projects() {
             <div className='projects-otsikko-container'>
                 <h1 className='projects-otsikko'>Projects</h1>
             </div>
-            <div>
-                
+            <div className='projects-grid'>
+                <div className='projects-wrapper'>
+                  <img src='logo192.png' className='project-image'></img>
+                </div>
+                <div className='projects-wrapper'>
+                  <img src='logo192.png' className='project-image'></img>
+                </div>
+                <div className='projects-wrapper'>
+                  <img src='logo192.png' className='project-image'></img>
+                </div>
             </div>
         </div>
     </div>
