a858b5db5bf2027d917169f5f8f16315921c53b3
WaterProofComputer <waterproofcomputer@gmail.com>
2024-08-14 11:57:55 +0300

    Update work.js

--- src/Components/Work/work.js
+++ src/Components/Work/work.js
@@ -31,6 +31,25 @@ function Work() {
             </div>
           </div>
           </div>
+          <div className='work-box'>
+          <div className='work-50'>
+            <div className='work-80'>
+            <h1 className='work-otsikko2'>KoGo Vision</h1>
+            <h1 className='work-otsikko2'>Internship, 22.4-30.9.2024</h1>
+            <p className='work-text'>I worked on an Azure web app project using MERN stack (MongoDB, Express.js, React, Node.js).
+               My tasks included working on both front-end and back-end development. I also participated in team meetings and project planning.
+            </p> 
+            </div>
+          </div>
+          <div className='work-50'>
+            <div className='work-80'>
+            <h1 className='work-otsikko2'>Ylikiimingin Koulu</h1>
+            <h1 className='work-otsikko2'>Internship, 2023</h1>
+            <p className='work-text'>I worked at Ylikiimingin Koulu. My tasks included fixing laptops, updating software and drivers, writing instructions for teachers on how to use projectors and smart boards, installing an operating system and fixing sound systems.
+            </p> 
+            </div>
+          </div>
+          </div>
       </div>
     </div>
   );
